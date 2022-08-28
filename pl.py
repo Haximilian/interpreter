68a0062da53eee24d487a1c682950bf13648d804
maximilian <Haximilian>
2022-08-27 20:29:29 -0700

    add define statement

--- pl.py
+++ pl.py
@@ -46,17 +46,54 @@ class If(Expression):
         else:
             return self.r.evaluate()
 
+SymbolTable = dict()
+
+# todo: this is not an expression
+class Define(Expression):
+    def __init__(self):
+        self.stub = None
+        self.function = None
+        
+    def parse(self, tokens):
+        self.stub = Stub()
+        self.stub.parse(tokens)
+        self.function = parse(tokens)
+
+        SymbolTable[self.stub.symbol] = self.function
+
+class Stub():
+    def __init__(self):
+        self.symbol = None
+        self.parameters = []
+    
+    def parse(self, tokens):
+        l = tokens.pop()
+        assert l == "(", f'"{l}" != "("'
+        self.symbol = tokens.pop()
+        next = None
+        while True:
+            next = tokens.pop()
+            if next == ")":
+                break
+            self.parameters.append(next)
+        assert next == ")", f'"{next}" != ")"'
+
 def createExpression(op) -> Expression:
     if op == "+":
         return Add()
     elif op == "if":
         return If()
+    elif op == "define":
+        return Define()
     else:
         raise Exception(f"{op} op does not exist")
 
 def parse(tokens):
     if tokens[-1].isdigit():
         return Integer(int(tokens.pop()))
+
+    if tokens[-1] in SymbolTable:
+        return SymbolTable[tokens.pop()]
 
     l = tokens.pop()
     assert l == "(", f'"{l}" != "("'
@@ -68,9 +105,12 @@ def parse(tokens):
 
     return e
 
-i = "( if 0 ( + ( + 32 8 ) ( + 12 ( + 3 ( + 7 8 ) ) ) ) 32 )"
+i = "( define ( hello ) 10 ) ( if 0 ( + ( + 32 8 ) ( + 12 ( + 3 ( + 7 8 ) ) ) ) hello )"
 
 tokens = i.split(" ")
 tokens.reverse()
-root = parse(tokens)
-print(f"{i} == {root.evaluate()}")
+root = []
+while tokens:
+    root.append(parse(tokens))
+for s in root:
+    print(f"{i} == {s.evaluate()}")
